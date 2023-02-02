8efcc15d67dc9522852ebc3386bce53119a8fd40
amangtele <122607345+amangtele@users.noreply.github.com>
2023-02-03 01:34:29 +0700

    Create asupan.py

---
+++ AmangRobot/modules/asupan.py
@@ -0,0 +1,9 @@
+__mod_name__ = "Asupan"
+
+__help__ = """
+
+*Asupan*
+❂ /ptl or /asupan: get random video from Instagram timeline
+❂ /chika: get random video from chikakiku
+❂ /boobs: coba aja sendiri.
+"""
