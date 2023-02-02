c0d36b2615ae62b36169fbb7863b4b6c0ea35637
amangtele <122607345+amangtele@users.noreply.github.com>
2023-02-03 02:36:15 +0700

    Update __main__.py

--- AmangRobot/__main__.py
+++ AmangRobot/__main__.py
@@ -111,7 +111,7 @@ buttons = [
     [
        InlineKeyboardButton(
            text=f"About {dispatcher.bot.first_name}", callback_data="amang_"),
-       InlineKeyboardButton=(text="☕️ Donate", url=f"https://t.me/{DONET}"),
+       InlineKeyboardButton(text="☕️ Donate", url=f"https://t.me/{DONET}"),
     ],
 ]
 
